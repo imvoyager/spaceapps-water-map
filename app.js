2213374d9e7fc8b0367f99195578f554e86d29b5
Julien <Julien@daniels-mbp.nhb-gb.ibmmobiledemo.com>
2017-04-29 21:51:32 +0100

    function for gmaps added

--- app.js
+++ app.js
@@ -2,11 +2,30 @@
  * Created by KNYordanov on 29.4.2017 г..
  */
 var gmaps = require('google-maps');
-
 var app = express();
+var async = require('async');
+var io;
 
 app.get('/', function (req, res) {
-    res.send('Hello World')
+    res.write('<html>\n<head>\n<style>\n')
+    res.write('#map { height: 400px; width: 100%;}\n')
+    res.write('</style>\n</head>\n')
+    res.write('<body>\n<div id="map"></div>\n')
+    res.write('<script>\n')
 });
 
 app.listen(3000);
+
+var req = request('https://maps.googleapis.com/maps/api/js?key=AIzaSyCQZ-jlREcmgiolyPb8qwIKH296-vwdNYI&callback=initMap')
+
+      function initMap() {
+        var uluru = {lat: -25.363, lng: 131.044};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
